5c6585413724369b45ddeef4da07d74b39ac96e2
Laila Viana <lailavalmeida@gmail.com>
2021-08-25 20:11:38 -0300

    importing datasets

--- file-test.R
+++ file-test.R
@@ -1,1 +1,7 @@
-test <- c(1,2,3)
+library(readr)
+child_mortality <- read_csv("C:/Users/lailaviana/Desktop/R class/projeto/child_mortality_0_5_year_olds_dying_per_1000_born.csv")
+vacc_rate <- read_csv("C:/Users/lailaviana/Desktop/R class/projeto/vacc_rate.csv")
+total_health <- read_csv("C:/Users/lailaviana/Desktop/R class/projeto/total_health_spending_percent_of_gdp.csv")
+medical_doctors <- read_csv("C:/Users/lailaviana/Desktop/R class/projeto/medical_doctors_per_1000_people.csv")
+life_expectancy <- read_csv("C:/Users/lailaviana/Desktop/R class/projeto/life_expectancy_years.csv")
+
